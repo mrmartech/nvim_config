71b1dc86e153a1fe8f7d54721cb8f44b6cd286bc
mrmartech <73462993+mrmartech@users.noreply.github.com>
2025-07-15 17:22:00 -0700

    Update packer.lua with enhanced Telescope setup

--- lua/hawaii/packer.lua
+++ lua/hawaii/packer.lua
@@ -4,11 +4,14 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-   use( 'nvim-tree/nvim-web-devicons' )    use( 'lewis6991/gitsigns.nvim' ) 
-    use( 'romgrk/barbar.nvim' )
- --Lexima 
- use( 'cohama/lexima.vim' )
- use({
+   use( 'nvim-tree/nvim-web-devicons' )    
+   use( 'lewis6991/gitsigns.nvim' ) 
+   use( 'romgrk/barbar.nvim' )
+   
+   --Lexima 
+   use( 'cohama/lexima.vim' )
+   
+   use({
     "kylechui/nvim-surround",
     tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
@@ -16,12 +19,15 @@ return require('packer').startup(function(use)
             -- Configuration here, or leave empty to use defaults
         })
     end
-})
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
- end)
-
-
+   })
+   
+   -- Telescope fuzzy finder
+   use {
+     'nvim-telescope/telescope.nvim', 
+     tag = '0.1.8',
+     requires = { 
+       {'nvim-lua/plenary.nvim'},
+       {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+     }
+   }
+end)
