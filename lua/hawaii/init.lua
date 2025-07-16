cfaa6c44e1d93ebc98aeea5934c1a087940bcc64
mrmartech <73462993+mrmartech@users.noreply.github.com>
2025-07-15 17:26:34 -0700

    Fix circular dependency by conditionally loading telescope

--- lua/hawaii/init.lua
+++ lua/hawaii/init.lua
@@ -1,4 +1,9 @@
 require("hawaii.set")
 require("hawaii.remap")
 require("hawaii.packer")
-require("hawaii.telescope")
+
+-- Load telescope config only if telescope is available
+local status_ok, telescope = pcall(require, "telescope")
+if status_ok then
+  require("hawaii.telescope")
+end
